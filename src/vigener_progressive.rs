f27cc7268bca8a5b6a439fd49e54dbe82d9797a3
tpouhuk <35433062+TpouHuK@users.noreply.github.com>
2023-03-02 14:22:45 +0300

    remove trait Rot

--- src/vigener_progressive.rs
+++ src/vigener_progressive.rs
@@ -13,42 +13,34 @@ fn filter_russian<I: Iterator<Item = char>>(inp: I) -> impl Iterator<Item = char
     })
 }
 
-trait Rot {
-    fn rot(self, num: u32) -> Self;
-    fn as_ru_u32(&self) -> u32;
-    fn from_ru_u32(value: u32) -> Self;
-}
-
 const ALPHABET: [char; 33] = [
     'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
     'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я',
 ];
 
-impl Rot for char {
-    fn rot(self, num: u32) -> Self {
-        Self::from_ru_u32((self.as_ru_u32() + num) % ALPHABET.len() as u32)
-    }
+fn rot(val: char, num: u32) -> char {
+    from_ru_u32((as_ru_u32(val) + num) % ALPHABET.len() as u32)
+}
 
-    fn as_ru_u32(&self) -> u32 {
-        if ('А'..='Е').contains(self) {
-            *self as u32 - 'А' as u32
-        } else if *self == 'Ё' {
-            dbg!('Е'.as_ru_u32() + 1)
-        } else if ('Ж'..='Я').contains(self) {
-            *self as u32 - 'А' as u32 + 1
-        } else {
-            unreachable!()
-        }
+fn as_ru_u32(val: char) -> u32 {
+    if ('А'..='Е').contains(&val) {
+        val as u32 - 'А' as u32
+    } else if val == 'Ё' {
+        as_ru_u32('Е') + 1
+    } else if ('Ж'..='Я').contains(&val) {
+        val as u32 - 'А' as u32 + 1
+    } else {
+        unreachable!()
     }
+}
 
-    fn from_ru_u32(value: u32) -> Self {
-        ALPHABET[value as usize]
-    }
+fn from_ru_u32(value: u32) -> char {
+    ALPHABET[value as usize]
 }
 
 impl VigenerProgressive {
     pub fn new(key: &str) -> Option<Self> {
-        let key: Vec<_> = filter_russian(key.chars()).map(|c| c.as_ru_u32()).collect();
+        let key: Vec<_> = filter_russian(key.chars()).map(as_ru_u32).collect();
 
         if key.is_empty() {
             return None;
@@ -74,7 +66,7 @@ impl VigenerProgressive {
                 }
             };
             let key_num = num + cycle_num;
-            text.push(char.rot(dbg!(key_num)));
+            text.push(rot(char, key_num));
         }
         text
     }
@@ -95,7 +87,7 @@ impl VigenerProgressive {
                     *key_iter.next().unwrap()
                 }
             };
-            text.push(char.rot(ALPHABET.len() as u32 - key_num));
+            text.push(rot(char, ALPHABET.len() as u32 - key_num));
         }
         text
     }
